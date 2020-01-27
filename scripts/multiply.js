cb3ed21bf406b7d913e8168e7943e7e85e702cbb
Mikhail Khokhlov <hazolsky@gmail.com>
2020-01-27 15:58:54 +0300

    Test

--- scripts/multiply.js
+++ scripts/multiply.js
@@ -4,7 +4,7 @@ var second = document.querySelector('#number2');
 var result1 = document.querySelector('.result1');
 
 if (!!window.SharedWorker) {
-  var myWorker = new SharedWorker("scripts/worker.js");
+  var myWorker = new SharedWorker("./worker.js");
 
   first.onchange = function() {
     myWorker.port.postMessage([first.value, second.value]);
